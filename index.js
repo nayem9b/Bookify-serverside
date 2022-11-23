06c018c6447c37ebb269612eff03af6392e6a722
nayem9b <razwanniam1@gmail.com>
2022-11-24 02:01:21 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
 app.get("/", (req, res) => {
   res.send("app is running");
 });
